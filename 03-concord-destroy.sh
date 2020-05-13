510993e8cbbe27e08bf3f3e9daf3b0b389e71b5f
Jason van Zyl <jason@vanzyl.ca>
2020-05-13 09:37:29 -0400

    Destroy the socat container if running

--- 03-concord-destroy.sh
+++ 03-concord-destroy.sh
@@ -6,5 +6,5 @@ then
     rm -rf $HOME/.kube_local
     rm -rf $HOME/.minikube_local
 fi
-docker rm -f agent server dind ${CONCORD_DB_NAME}
+docker rm -f agent server dind socat ${CONCORD_DB_NAME}
 docker volume rm -f $CONCORD_DB_NAME
